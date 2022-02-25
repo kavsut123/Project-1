4ececf1e98fd628e7c44ea9ea549d2bcc94d83c7
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-02-25 16:56:03 +0000

    Imporved TypeII Error function to icluded discrepency between one sided and two sided tests.

--- Type II Error Function.R
+++ Type II Error Function.R
@@ -1,10 +1,16 @@
-type_II <- function(quant, delta, var, n, r) {
+type_II <- function(alpha, delta, var, n, r, two_sided = FALSE) {
   # Calculates type II error of a z-test of means given
-  # quant - the critical value
+  # alpha - the nominal type I error rate
   # delta - the hypothesised mean difference
   # var - the variance of the continuous outcome
   # n - the sample size in group 1
-  # r- represents the ratio between the two sample sizes.
+  # r- represents the ratio between the two sample sizes n1 and n2.
+  # two_sided -TRUE indicates a two-sided test is needed, FALSE a 
+  # one-sided test.
+  
+  # Get the critical value to use in the hypothesis test.
+  quant <- qnorm(p = 1 - alpha, mean = 0, sd = 1)
+  if(two_sided) quant <- qnorm(p = 1 - alpha/2, mean = 0, sd = 1)
   
   tII <- pnorm(q = quant - delta / sqrt(var / n + var / ceiling(r*n)), mean = 0, sd = 1)
   return(tII)
