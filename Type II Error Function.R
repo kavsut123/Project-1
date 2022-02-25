c8cc8d7801b04f04232deda313c740cebb8a2792
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-02-25 16:51:20 +0000

    Improved function for the Type II Error of a hypothesis test.

--- Type II Error Function.R
+++ Type II Error Function.R
@@ -1,13 +1,11 @@
-type_II <- function(quant, delta, var, n) {
+type_II <- function(quant, delta, var, n, r) {
   # Calculates type II error of a z-test of means given
   # quant - the critical value
   # delta - the hypothesised mean difference
   # var - the variance of the continuous outcome
-  # n - the sample size is each group
+  # n - the sample size in group 1
+  # r- represents the ratio between the two sample sizes.
   
-  tII <- pnorm(q = quant - delta / sqrt(2 * var / n), mean = 0, sd = 1)
+  tII <- pnorm(q = quant - delta / sqrt(var / n + var / ceiling(r*n)), mean = 0, sd = 1)
   return(tII)
 }
-
-# For example,
-type_II(quant = 1.64, delta = 0.3, var = 1, n = 100)
