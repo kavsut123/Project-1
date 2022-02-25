e37887908f5e79844a1cf00629786acc91fb29aa
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-02-25 17:52:49 +0000

    Monte Carlo estimate for theta

--- Formative Assessment 1.R
+++ Formative Assessment 1.R
@@ -1,7 +1,15 @@
 set.seed(10000)
 
-#Parameters: Here we set out what our parameters will be.
-lambda=0.5
-gamma=0.8
-n1=60
-n2=30
+#Parameters needed for estimating theta.
+N=10000 #Number of realisations in the monte carlo estimate.
+a=0.5   #The initial value of a in the prior.
+b=0.5   #The initial value of b in the prior.
+
+#Use monte carlo estimation to produce an estimate for theta.
+thetas=0
+for(i in 1:N){
+  theta=rbeta(1, 0.5, 0.5)
+  thetas=thetas+theta
+}
+
+theta.est=thetas/N
