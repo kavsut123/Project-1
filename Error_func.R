57433c9af8a3cdb2c00a539b89523b07b4c594c2
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-03-12 11:42:36 +0000

    A function for simulating type I and II error

---
+++ Error_func.R
@@ -0,0 +1,17 @@
+Error_func=function(N,theta,c,type){
+  #N is the number needed in the Monte Carlo Estimate
+  #Theta is the value of theta we are using.
+  #c is the critical value
+  #type is the type of erroe we want
+  Error=numeric(100)
+  for(i in 1:100){
+    x=rbinom(N,i,theta)
+    if(type==1){
+      Error[i]=mean(x>=c*i)
+      
+    }
+    if(type==2){
+      Error[i]=1-mean(x>=c*i)
+    }
+  }
+}
