4a20ec1f17d91ee4ae151e02f9a0b7f2f703a911
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-03-12 12:13:54 +0000

    Further improved code for simulating the sample size uder the two hypotheses.

--- Summative Assessment.R
+++ Summative Assessment.R
@@ -1,9 +1,9 @@
 set.seed(4287248)
 #Parameters
 #Theta Under the Null
-theta0=0.5
+theta.null=0.5
 #Theta under the Alternate.
-theta1=0.7
+theta.alt=0.7
 #Type 1 error under theta=0.5, want alpha<0.05
 alpha=0.05
 #Type II error under theta=0.7, want beta<0.2
@@ -13,32 +13,27 @@ beta=0.2
 
 #Investigating the size of the first Trial
 N=100000
-p=1
-n1=1
-#This loop runs whilst the type I error, p, is above 0.05
-while(p>0.05){
-  
-  #We choose some Critical value, c, here I have chosen
-  #to use 0.7 as we will only be proceeding if a high proportion
-  #of the sample respond, thus we need to know what sample size 
-  #is needed to reduce the sample size to below 0.05.
-  c=0.7*n1
-  
-  x=rbinom(N,n1,theta0)
-  
-  #Here, p is the Type I error.
-  p=mean(x>=c)
-  n1=n1+1
-}
 
-#Now we have a rough idea of what n1, should be we test sample
-#size a bit above to give us a better idea of what n1 should be.
-Error=numeric(30)
-for(i in 1:30){
-  x=rbinom(N,i,theta0)
-  Error[i]=mean(x>=0.7*i)
-}
+
+#We choose some Critical value, c, here I have chosen
+#to use 65% of the size as we will only be proceeding if a high proportion
+#of the sample respond, thus we need to know what sample size 
+#is needed to reduce the type I error to below 0.05.
+Error=Error_func(N,theta.null,0.65,type=1)
+
 
 #Here we plot the type I errors, an plot a line in red for error=0.05.
-plot(Error, xlab="Sample Size of Phase 1", ylab="Type I Error",main="The Tye I error rates of each phase one sample size")
+plot(Error, xlab="Sample Size", ylab="Type I Error",main="The Type I error rates of each sample size")
 abline(h=0.05, col="red")
+abline(v=40,lty=2)
+legend(65,0.5,legend = c("Error=0.05","Sample Size of 40"),col=c("red","black"),lty=c(1,2))
+
+#Now for the Type II Error under theta.alt.
+N=100000
+
+
+Error=Error_func(N,theta.alt,0.65,type=2)
+plot(Error, xlab="Sample Size",ylab="Type II Error",main="The Type II error rates of each sample size")
+abline(h=0.2, col="red")
+abline(v=75,lty=2)
+legend(65,0.5,legend = c("Error=0.2","Sample Size of 75"),col=c("red","black"),lty=c(1,2))
