2b6d90136454b9633a9e4458085750e4c8cb24a7
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-03-14 13:14:33 +0000

    Analysising small values of lambda

--- Analysis_of_descision_parameters.R
+++ Analysis_of_descision_parameters.R
@@ -16,3 +16,11 @@ for(i in 1:nrow(values)) {
 B=matrix(exp_n,nrow=21,ncol=21)
 B=data.frame(B,row.names=seq(0, 1, 0.05))
 colnames(B)=seq(0, 1, 0.05)
+
+
+#Analysis of values below 0.05
+values=expand.grid(lambda = seq(0, 0.05, 0.01),gamma =0)
+exp_n= numeric(nrow(values))
+for(i in 1:nrow(values)) {
+  exp_n[i] <- evaluate_design(values[i, 1], values[i, 2], n1 = n1, n2 = n2)[1]
+}
