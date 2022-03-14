128ae1c2a97a7e49679b32e0c23ecc8c03d88101
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-03-14 12:40:27 +0000

    Adds a data frame to store the data in

--- Analysis_of_descision_parameters.R
+++ Analysis_of_descision_parameters.R
@@ -10,4 +10,9 @@ exp_n= numeric(nrow(values))
 for(i in 1:nrow(values)) {
   exp_n[i] <- evaluate_design(values[i, 1], values[i, 2], n1 = n1, n2 = n2)[1]
 }
-exp_n
+
+
+#Makes a data frame B to make the data more user friendly
+B=matrix(exp_n,nrow=21,ncol=21)
+B=data.frame(B,row.names=seq(0, 1, 0.05))
+colnames(B)=seq(0, 1, 0.05)
