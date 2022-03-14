2602b5fdaea9cae37bd8a8f6e13c498fd04a54aa
Kavan Sutcliffe <kavsut@hotmail.co.uk>
2022-03-14 11:58:52 +0000

    probabilioty function for values of y1

---
+++ y1_prob.R
@@ -0,0 +1,6 @@
+y1_prob= function(y1, n1) {
+  # Calculate the probability of observing y_1 responses in n_1 trials
+  # under a Beta-binomial model with Beta(a = 0.5, b = 0.5) prior.
+  
+  choose(n1, y1) * beta(y1 + 0.5, n1 - y1 + 0.5) / beta(0.5, 0.5)
+}
